e8ac111b3bfe424265270fad82eb98ecf94495ff
pitbrest <sasha.petrov@tut.by>
2022-05-01 22:51:31 +0300

    some commit

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,31 +6,98 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
 class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+   this.root = addInto(this.root, value);
+	 
+	  function addInto(node, value) {
+		  if (!node) {
+			  return new Node(value);
+		  }
+		  if (node.value === value) {
+			  return node;
+		  }
+
+		  if (value < node.value) {
+			node.left = addInto(node.left, value);
+			} else {
+				node.right = addInto(node.right, value);
+			}
+			return node;
+	  }	  
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  has(value) {
+		return addInto(this.root, value);
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		function addInto(node, value) {
+			if (!node) {
+				return false;
+			}
+			if (node.value === value) {
+				return true;
+			}
+			return value < node.value ?
+				addInto(node.left, value) :
+				addInto(node.right, value);
+		}
   }
 
   find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		throw new NotImplementedError('Not implemented');
+		// remove line with error and write your code here
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(value) {
+   	this.root = removeNode(this.root, value);
+
+		function removeNode(node, value) {
+			if (!node) {
+				return null;
+			}
+			if (value < node.value) {
+				node.left = removeNode(node.left, value);
+				return node;
+			} else if (node.value < value) {
+				node.right = removeNode(node.right, value);
+				return node;
+			} else {
+				if (!node.left && !node.right) {
+					return null
+				}
+				if (!node.left) {
+					node = node.right;
+					return node;
+				}
+				if (!node.right) {
+					node = node.left;
+					return node;
+				}
+
+				let minFromRight = node.right;
+
+				while(minFromRight.left) {
+					minFromRight = minFromRight.left;
+				}
+				node.value = minFromRight.value;
+				node.right = removeNode(node.right, minFromRight.value);
+
+				return node
+			}			
+		}
   }
 
   min() {
